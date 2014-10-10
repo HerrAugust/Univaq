86d02fc34d8b465f4f646b17266c88d7d9b2e81b
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 14:36:39 +0200

    Create Consegna1-array.cpp

---
+++ Array/Consegna1/Consegna1-array.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name : Consegna1-cicliforewhile.cpp
+// Author : Agostino Mascitti
+// Email : agostino_aless@yahoo.it
+// Version :
+// Copyright : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+// Consegna: Inventa 5 numeri e stampali a video moltiplicati per 3.
+// Nota: In questo progetto viene usato di fatto il C, ma ho creato un progetto C++ per usare cout e cin.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+  return 0;
+}
