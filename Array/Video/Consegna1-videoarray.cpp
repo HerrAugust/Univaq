86d03a658fb154be288f18e33fb0a61734f68368
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 21:45:46 +0200

    Create Consegna1-videoarray.cpp

---
+++ Array/Video/Consegna1-videoarray.cpp
@@ -0,0 +1,28 @@
+//============================================================================
+// Name        : Consegna1-videoarray.cpp
+// Author      : Agostino Mascitti
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+// Consegna    : Inventare 5 interi e stamparli a video
+// Questo codice stampa "Linea eseguita" tante volte quanto l'utente digita in console finchè non scrive "0".
+
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	//Inventare 5 interi
+	int mioArray[5] = { 21, 2, 9, -9, 9 };
+
+	//stamparli a video
+	cout << mioArray[0] << endl;
+	cout << mioArray[1] << endl;
+	cout << mioArray[2] << endl;
+	cout << mioArray[3] << endl;
+	cout << mioArray[4] << endl;
+
+	return 0;
+}
