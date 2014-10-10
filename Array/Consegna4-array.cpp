b40120484870ddc5767411c5190ed38872c6f220
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 15:17:10 +0200

    Update Consegna4-array.cpp

--- Array/Consegna4-array.cpp
+++ Array/Consegna4-array.cpp
@@ -5,6 +5,29 @@
 // Version :
 // Copyright : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-// Consegna: Chiedere all’utente 3 numeri e stamparne il moltiplicato per 2
+// Consegna: Chiedere il valore di n all’utente. Chiedere 20 interi all’utente e stamparne il moltiplicato per n.
 // Nota: In questo progetto viene usato di fatto il C, ma ho creato un progetto C++ per usare cout e cin.
 //============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //Chiedo n
+  int n = 0;
+  cout << "n = ";
+  cin >> n;
+  
+  //Chiedo 20 interi all'utente
+  int num[20];
+  for (int i = 0; i < 20; i++) {
+    cout << "Valore " << i+1 << " di 20: ";
+    cin >> num[i];
+  }
+  
+  for(int i = 0; i < 20; i++)
+    cout << num[i] << " * " << n << " = " << num[i]*n << endl;
+  
+  return 0;
+}
