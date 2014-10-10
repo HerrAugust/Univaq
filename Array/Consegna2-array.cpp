2933b70f68f2e570581571d914225da6561c8b91
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 14:59:06 +0200

    Update Consegna2-array.cpp

--- Array/Consegna2-array.cpp
+++ Array/Consegna2-array.cpp
@@ -5,6 +5,26 @@
 // Version :
 // Copyright : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-// Consegna: Chiedere all’utente 3 numeri e stamparne il moltiplicato per 2
+// Consegna: Chiedi all’utente 5 numeri e stampali a video moltiplicati per 3
 // Nota: In questo progetto viene usato di fatto il C, ma ho creato un progetto C++ per usare cout e cin.
 //============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //Chiedo 5 numeri e li salvo in un array
+  int num[5];
+  cout << "Scrivi i numeri (5): " << endl;
+  cin >> num[0] >> num[1] >> num[2] >> num[3] >> num[4];
+  
+  cout << endl;
+  cout << num[0] << " * 3 = " << num[0] * 3 << endl;
+  cout << num[1] << " * 3 = " << num[1] * 3 << endl;
+  cout << num[2] << " * 3 = " << num[2] * 3 << endl;
+  cout << num[3] << " * 3 = " << num[3] * 3 << endl;
+  cout << num[4] << " * 3 = " << num[4] * 3 << endl;
+  
+  return 0;
+}
