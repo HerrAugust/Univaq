43d27850da1346c54581cca4d84e8563b3275578
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 15:24:42 +0200

    Update Consegna5-array.cpp

--- Array/Consegna5-array.cpp
+++ Array/Consegna5-array.cpp
@@ -5,6 +5,31 @@
 // Version :
 // Copyright : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-// Consegna: Chiedere all’utente 3 numeri e stamparne il moltiplicato per 2
+// Consegna: Chiedere all’utente 2 valori. Chiedere 20 numeri all’utente e stamparne il moltiplicato con i 2 valori, prima uno e poi l’altro.
 // Nota: In questo progetto viene usato di fatto il C, ma ho creato un progetto C++ per usare cout e cin.
 //============================================================================
+
+int main() {
+  
+  //Chiedo 2 valori
+  int moltiplicatori[2];
+  cout << "Inserire 2 interi: ";
+  cin >> moltiplicatori[0] >> moltiplicatori[1];
+  
+  //Chiedo 20 interi
+  int num[20];
+  for(int i = 0; i < 20; i++) {
+    cout << "Inserire un intero ";
+    cin >> num[i];
+    cout << endl;
+  }
+  
+  for(int i = 0; i < 20; i++) {
+    if(i % 2 == 0) //% restituisce il modulo, cioè il resto della divisione. In questo caso mi chiedo se i è pari
+      cout << num[i] * moltiplicato[0];
+    else
+      cout << num[i] * moltiplicato[1];
+  }
+  
+  return 0;
+}
