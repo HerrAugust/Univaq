3ccfac1a62237ea46ff23a8fbd7dfd86d526919c
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 15:04:57 +0200

    Update Consegna3-array.cpp

--- Array/Consegna3-array.cpp
+++ Array/Consegna3-array.cpp
@@ -5,6 +5,31 @@
 // Version :
 // Copyright : Your copyright notice
 // Description : Hello World in C++, Ansi-style
-// Consegna: Chiedere all’utente 3 numeri e stamparne il moltiplicato per 2
+// Consegna: Chiedi all’utente 5 numeri e stampa a video il moltiplicato per il numero precedente (per n = 1 stampa il valore stesso)
 // Nota: In questo progetto viene usato di fatto il C, ma ho creato un progetto C++ per usare cout e cin.
 //============================================================================
+
+
+//Prerequito addizionale: if-else
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  
+  //Chiedo 5 numeri e li memorizzo in un array
+  int num[5];
+  cout << "Inserisci 5 numeri: " << endl;
+  cin >> num[0] >> num[1] >> num[2] >> num[3] >> num[4];
+  
+  //Stampo come in consegna (vedi sopra)
+  cout << endl;
+  cout << num[0] << endl; //Perchè non posso trattare num[0] come tutti gli altri elementi dell'array?
+  cout << num[0] * num[1] << endl;
+  cout << num[1] * num[2] << endl;
+  cout << num[2] * num[3] << endl;
+  cout << num[3] * num[4] << endl;
+  //Perchè non posso stampare num[4] * num[5]?
+  
+  return 0;
+}
