757a5e4d6b9124c9340d2a789447a0f7528a9625
HerrAugust <HerrAugust@users.noreply.github.com>
2014-10-10 14:40:53 +0200

    Update Consegna1-array.cpp

--- Array/Consegna1/Consegna1-array.cpp
+++ Array/Consegna1/Consegna1-array.cpp
@@ -14,6 +14,21 @@ using namespace std;
 
 int main() {
 
+  //Invento 5 numeri e li memorizzo in un array
+  int numeri[5] = { 4, 20, 1, 0 }; //nota: essendo l'inizializzazione contestuale alla dichiarazione di numeri[], avrei potuto scrivere semplicemente int numeri[5] = { 4, 20, 1, 0 };
+  
+  //Moltiplico i numeri per 3 e li stampo a video
+  cout << numeri[0] << "*3 = " << numeri[0] * 3 << endl;
+  cout << numeri[1] << "*3 = " << numeri[1] * 3 << endl;
+  cout << numeri[2] << "*3 = " << numeri[2] * 3 << endl;
+  cout << numeri[3] << "*3 = " << numeri[3] * 3 << endl;
+  cout << numeri[4] << "*3 = " << numeri[4] * 3 << endl;
+  
+  /*
+  * Cosa devi notare:
+  * 1. sono partito da numeri[0] e non da numeri[1]
+  * 2. sono arrivato fino a numeri[4] e non fino a numeri[5]
+  */
 
   return 0;
 }
