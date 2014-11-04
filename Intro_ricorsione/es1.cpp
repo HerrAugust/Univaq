429dc0891a7200074b8a1e2c7a6097aa253a362a
HerrAugust <HerrAugust@users.noreply.github.com>
2014-11-04 22:57:50 +0100

    Update es1.cpp

--- Intro_ricorsione/es1.cpp
+++ Intro_ricorsione/es1.cpp
@@ -2,13 +2,15 @@
 
 #include <iostream>
 
+using namespace std;
+
 //versione ricorsiva basata su iterativa col while
 void versioneRicorsiva(int i) {
   if(i == 0) return; //falsifico la condizione del while. In questo caso il while termina e la funzione restituisce nell'iterativo (return;)
   
   //questo è ciò che succedeva quando la condizione del while era vera. Caso i < 10!
   cout << "Hello world" << endl;
-  versioneRicorsiva(i+1); //attenzione: NON i++ o ++i!
+  return versioneRicorsiva(i+1); //attenzione: NON i++ o ++i!
   
 }
 
