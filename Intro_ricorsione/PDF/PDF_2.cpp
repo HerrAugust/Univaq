27bf10b4d04f4f0bb6e87b39e93e28e8d65bf912
HerrAugust <HerrAugust@users.noreply.github.com>
2014-11-20 19:44:47 +0100

    Update PDF_2.cpp

--- Intro_ricorsione/PDF/PDF_2.cpp
+++ Intro_ricorsione/PDF/PDF_2.cpp
@@ -7,10 +7,11 @@ int num[12] { 3,2,0,9,0,-1,-1,10,2,7,6,8 };
 //Questa è la funzione ricorsiva
 int metRic(int vsa, int minimo) {
  if(vsa == 12) return minimo;
- if(num[vsa] < minimo)
- minimo = num[vsa];
+ if(num[vsa] < minimo) {
+  minimo = num[vsa];
+ }
  vsa++;
- return metRic(num, vsa, minimo);
+ return metRic(vsa, minimo);
 }
 
 /* Questa è la funzione iterativa
@@ -29,8 +30,8 @@ cout << minimo << endl;
 int main(void) {
  int minimo = num[0];
  varScorrArray = 0;
- metRic(varScorrArray, minimo);
+ minimo = metRic(varScorrArray, minimo);
  cout << minimo << endl;
- //funzIter() Avrai lo stesso output
+ //funzIter(); Avrai lo stesso output
  return 0;
 }
