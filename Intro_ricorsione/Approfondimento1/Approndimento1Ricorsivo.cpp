840707c90145cc43073dc937d2f11e0a7b95729c
HerrAugust <HerrAugust@users.noreply.github.com>
2014-11-20 23:18:28 +0100

    Create Approndimento1Ricorsivo.cpp

---
+++ Intro_ricorsione/Approfondimento1/Approndimento1Ricorsivo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void versioneRicorsiva(int varScorr, int maggiore, int minore) {
+	if(varScorr == 5) {
+		cout << "il maggiore è " << maggiore << endl;
+		return;
+	}
+	if(numeri[varScorr] < minore)
+		minore = numeri[varScorr];
+	if(numeri[varScorr] > maggiore)
+		maggiore = numeri[varScorr];
+	varScorr++;
+	return versioneRicorsiva(varScorr, maggiore, minore);
+}
+
+int main(void) {
+
+	versioneIterativa();
+
+	//Nota: questo ciclo non va trasformato in ricorsivo per consegna
+	for(int i = 0; i < 5; i++) {
+		cout << "Inserisci un numero intero: " << endl;
+		cin >> numeri[i];
+	}
+	int minore = numeri[0];
+	int maggiore = numeri[0];
+	int varScorr = 0;
+	versioneRicorsiva(varScorr, maggiore, minore);
+
+	return 0;
+}
