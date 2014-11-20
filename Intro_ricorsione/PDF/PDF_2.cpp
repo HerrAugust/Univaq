2024135096c60982972a89ffa30a2eb8bc496786
HerrAugust <HerrAugust@users.noreply.github.com>
2014-11-20 19:23:56 +0100

    Create PDF_2.cpp

---
+++ Intro_ricorsione/PDF/PDF_2.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+//Sopra sia alla funzione ricorsiva sia alla iterativa, perchè in entramber num[] deve essere visibile
+int num[12] { 3,2,0,9,0,-1,-1,10,2,7,6,8 };
+
+//Questa è la funzione ricorsiva
+int metRic(int vsa, int minimo) {
+ if(vsa == 12) return minimo;
+ if(num[vsa] < minimo)
+ minimo = num[vsa];
+ vsa++;
+ return metRic(num, vsa, minimo);
+}
+
+/* Questa è la funzione iterativa
+void funzIter() {
+int minimo = num[0];
+varScorrArray = 0;
+while(varScorrArray < 12) {
+ if(num[varScorrArray] < minimo)
+ minimo = num[varScorrArray];
+ varScorrArray++;
+}
+cout << minimo << endl;
+}
+*/
+
+int main(void) {
+ int minimo = num[0];
+ varScorrArray = 0;
+ metRic(varScorrArray, minimo);
+ cout << minimo << endl;
+ return 0;
+}
