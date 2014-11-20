3f098769a6cb63e1b9ff3a1c348d137ed3794548
HerrAugust <HerrAugust@users.noreply.github.com>
2014-11-20 23:17:46 +0100

    Create Approndimento1Iterativo.cpp

---
+++ Intro_ricorsione/Approfondimento1/Approndimento1Iterativo.cpp
@@ -0,0 +1,24 @@
+void versioneIterativa() {
+int numeri[18];
+//Nota: questo ciclo non va trasformato in ricorsivo per consegna
+for(int i = 0; i < 18; i++) {
+cout << "Inserisci un numero intero: " << endl;
+cin >> numeri[i];
+}
+int minore = numeri[0];
+int maggiore = numeri[0];
+for(int varScorr = 0; varScorr < 18; varScorr++) { //Trasformo prima in while per essere facilitato
+if(numeri[varScorr] < minore)
+minore = numeri[varScorr];
+if(numeri[varScorr] > maggiore)
+maggiore = numeri[varScorr];
+} //nova iterazione => nuova chiamata ricorsiva
+cout << "il maggiore è " << maggiore << endl;
+}
+
+
+int main(void)
+{
+  versioneIteratva();
+  return 0;
+}
